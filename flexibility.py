9dd18b1951595a5d091065b3439be9a5f983c8a3
suadou <92791932+suadou@users.noreply.github.com>
2022-03-08 12:58:52 +0100

    Add files via upload

--- flexibility.py
+++ flexibility.py
@@ -2,13 +2,34 @@ import argparse
 from PDB import request
 from FASTA import read
 import threading
+import configparser
 
 
-def AmphiProt(query):
-    for seq in request.parse_XML(request.BLAST_sp(query)):
+def AmphiProt(query, local, database=None):
+    if local == False:
+        BLAST = request.BLAST_sp(query)
+    else:
+        BLAST = request.BLAST_commandline(query, database)
+    for seq in request.parse_XML(BLAST):
         if request.download_AlphaFold(query, seq[0], './files/PDB/') == False:
             continue
         else:
+            AlphaFold_list.append(request.PDB(
+                query.identifier, seq[0], './files/PDB/' + query.identifier + '_AlphaFold.pdb', seq[1], seq[2], seq[3], seq[4]))
+            break
+
+
+def PDB(query, local, database=None):
+    if local == False:
+        BLAST = request.BLAST_PDB(query)
+    else:
+        BLAST = request.BLAST_commandline(query, database)
+    for seq in request.parse_XML(BLAST):
+        if request.download_PDB(query, seq[0], './files/PDB/') == False:
+            continue
+        else:
+            PDB_list.append(request.PDB(
+                query.identifier, seq[0], './files/PDB/' + query.identifier + '.pdb', seq[1], seq[2], seq[3], seq[4]))
             break
 
 
@@ -24,19 +45,35 @@ parser.add_argument('--output_png', dest='output_png_file', action='store',
                     help='Protein flexibility graphycal representation file.', required=False, default='flexibility_png_output')
 options = parser.parse_args()
 
+config = configparser.ConfigParser()
+
 print(f"Reading {options.input_file} file.")
 fasta_list = read.FASTA_parser(options.input_file)
 print(f"{len(fasta_list)} record/s was/were read.")
 
 # Threading for API BLASTp search
-threads = len(fasta_list)
-jobs = []
-for i in range(0, threads):
-    thread = threading.Thread(target=AmphiProt(fasta_list[i]))
-    jobs.append(thread)
+if config['blast']['local'] == False:
+    threads = len(fasta_list)
+    jobs = []
+    PDB_list = []
+    AlphaFold_list = []
+    for i in range(0, threads):
+        thread = threading.Thread(target=AmphiProt(fasta_list[i], False))
+        jobs.append(thread)
+        thread = threading.Thread(target=PDB(fasta_list[i], False))
+        jobs.append(thread)
 
-for j in jobs:
-    j.start()
+    for j in jobs:
+        j.start()
 
-for j in jobs:
-    j.join()
+    for j in jobs:
+        j.join()
+
+else:
+    PDB_list = []
+    AlphaFold_list = []
+    for seq in fasta_list:
+        PDB(seq, True, config['blast']['PDBdb_path'])
+        AmphiProt(seq, True, config['blast']['SwissProtdb_path'])
+
+print(PDB_list)
