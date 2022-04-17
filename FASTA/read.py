75d1004a96b4d1babaa3d3edeebce51d3d56544c
silgl <93728930+silgl@users.noreply.github.com>
2022-04-17 22:10:59 +0200

    Update read.py

--- FASTA/read.py
+++ FASTA/read.py
@@ -1,25 +1,20 @@
 """
-read module read a FASTA or multi-FASTA protein file and returns a Query object
+Read a FASTA or multi-FASTA protein file and returns a Query object
 for each sequence introduced.
 """
 from Bio import SeqIO
 
-
 class Query(object):
-    """
-    Query object stores the identifier and the sequence of a FASTA protein
-    sequence.
-    """
+    """Stores the identifier and the sequence of a FASTA protein sequence."""
 
     def __init__(self, sequence, identifier):
         self.sequence = str(sequence)
         self.identifier = identifier
 
-
 def fasta_parser(input_file):
     """
-    fasta_parser uses a FASTA or multi-FASTA protein file path and returns a list
-    of Query objects of each record.
+    Takes a FASTA or multi-FASTA protein file path and returns a list
+    of Query objects for each record.
     """
     fasta_sequence = SeqIO.parse(open(input_file), 'fasta')
     query_list = []
